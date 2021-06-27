d5f0a8d25cfe05a9af5c8c6230041adfdae713e5
Chris Brousseau <cbrews@users.noreply.github.com>
2021-06-27 17:28:05 -0400

    Making setup.py more dynamic (#24)

--- setup.py
+++ setup.py
@@ -10,28 +10,15 @@ from setuptools import find_packages, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
-short_description = "ignition - Gemini Protocol Client Transport Library"
 long_description = (here / 'README.md').read_text(encoding='utf-8')
+requirements = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
 
 setup(
-  name='ignition-gemini',
-  version='0.1.6',
-  description=short_description,
   long_description=long_description,
   long_description_content_type='text/markdown',
-  license='MPL 2.0',
-  url='https://github.com/cbrews/ignition',
-  author='Chris Brousseau',
-  classifiers=[
-    'Development Status :: 2 - Pre-Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Libraries',
-    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-    'Programming Language :: Python :: 3 :: Only',
-  ],
-  keywords='gemini, client, request, socket',
   packages=find_packages(exclude=["tests",
-                                  "tests.*"]),
+                                  "tests.*",
+                                  "examples"]),
   python_requires='>=3.7, <4',
-  install_requires=['cryptography>=3.0']
+  install_requires=requirements
 )
